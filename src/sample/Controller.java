141ee1cd0f23d7c1f67988387c88007d65ad55df
gpontejos <gp1045@txstate.edu>
2017-03-07 17:10:23 -0600

    Greg branch. Added scanner

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -13,12 +13,20 @@ import jpcap.NetworkInterface;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.control.TextArea;
+import jpcap.NetworkInterfaceAddress;
+import jpcap.packet.Packet;
 
 
 public class Controller implements Initializable {
 
     NetworkInterface[] devices = JpcapCaptor.getDeviceList();
     public ComboBox deviceList;
+
+    @FXML
+    public TextArea consoleOutput = new TextArea();
+
+    public int captureStatus;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -31,13 +39,13 @@ public class Controller implements Initializable {
     }
 
     @FXML
-    public void closeApp() {
+    void closeApp() {
         Platform.exit();
         System.exit(0);
     }
 
     @FXML
-    public void handleAbout() {
+    void handleAbout() {
         GridPane root = new GridPane();
         Stage aboutPopup = new Stage();
         try {
@@ -62,7 +70,69 @@ public class Controller implements Initializable {
         interfacePopUp.setScene(new Scene(pane, 600,400));
         interfacePopUp.show();
 
-        System.out.print("Interface List PopUp Launched");
+        System.out.print("Interface List PopUp Launched\n");
+    }
+
+    public NetworkInterface getSelectedInterface () {
+        int r = deviceList.getSelectionModel().getSelectedIndex();
+
+        System.out.printf("Device selected: %s\n", r);
+
+        return devices[r];
+    }
+
+    @FXML
+    public void startCapture() throws IOException {
+
+        if (deviceList.getSelectionModel().getSelectedIndex() != -1) {
+            new Thread(() -> capturePackets()).start();
+        } else {
+            System.out.printf("Error: Interface not selected.\n");
+        }
+    }
+
+    public void capturePackets() {
+        captureStatus = 0;
+
+        JpcapCaptor captor = null;
+
+        try {
+            captor = JpcapCaptor.openDevice(getSelectedInterface(), 65535, false, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("Network Interface opened\n");
+
+        // Capture packets until stopCapture is called
+
+        while (captureStatus == 0) {
+            printPacket(captor.getPacket());
+        }
+        captor.close();
+    }
+
+
+    public void printPacket(Packet packet) {
+        Platform.runLater(() -> {
+            if (packet != null) {
+                consoleOutput.appendText(String.valueOf(packet));
+                consoleOutput.appendText("\n");
+                System.out.printf("%s\n", String.valueOf(packet));
+            }
+        });
+
+    }
+
+    @FXML
+    public void stopCapture() {
+        captureStatus = 1;
+        System.out.printf("Capture ended.\n");
+    }
+
+
+    @FXML
+    public void clearCapture() {
+        consoleOutput.clear();
     }
 }
 
